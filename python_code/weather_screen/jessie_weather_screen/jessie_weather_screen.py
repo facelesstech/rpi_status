a732a672ba416a5cf3d6a8ea4cf48baab94deb89
facelessloser <facelessloser@gmail.com>
2016-10-07 16:03:01 +0100

    turned the ws2812 brightness down

--- python_code/weather_screen/jessie_weather_screen/jessie_weather_screen.py
+++ python_code/weather_screen/jessie_weather_screen/jessie_weather_screen.py
@@ -16,8 +16,8 @@ LED_COUNT = 8 # Number of LED pixels.
 LED_PIN = 12 # GPIO pin connected to the pixels (must support PWM!) 
 LED_FREQ_HZ = 800000 # LED signal frequency in hertz (usually 800khz)
 LED_DMA = 5 # DMA channel to use for generating signal (try 5)
-#LED_BRIGHTNESS = 127 # Set to 0 for darkest and 255 for brightest
-LED_BRIGHTNESS = 255 # Set to 0 for darkest and 255 for brightest
+LED_BRIGHTNESS = 127 # Set to 0 for darkest and 255 for brightest
+#LED_BRIGHTNESS = 255 # Set to 0 for darkest and 255 for brightest
 LED_INVERT = False # True to invert the signal (when using NPN transistor level shift)
 
 strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
